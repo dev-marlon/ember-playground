491f2e4862a7a411773c8f2ccae10f6b7673684c
Marlon <info@t3labs.de>
2015-06-07 17:15:32 +0200

    test routing

---
+++ app/routes/post.js
@@ -0,0 +1,24 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+    posts: [{
+            uid: 1,
+            title: 'Dies ist eine Überschrift',
+            bodytext: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        }, {
+            uid: 2,
+            title: 'Dies ist auch eine Überschrift',
+            bodytext: 'Lorem ipsum dolor sit amet sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        }, {
+            uid: 3,
+            title: 'Dies ist auch noch eine Überschrift',
+            bodytext: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor mollit anim id est laborum.'
+    }],
+    model: function(params) {
+        return this.get('posts').filter(function(el, index){
+            if(el.uid === parseInt(params.post_id)){
+                return el;
+            }
+        })[0];
+    }
+});
---
+++ app/routes/posts.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  posts: [{
+          uid: 1,
+          title: 'Dies ist eine Überschrift',
+          bodytext: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+      }, {
+          uid: 2,
+          title: 'Dies ist auch eine Überschrift',
+          bodytext: 'Lorem ipsum dolor sit amet sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+      }, {
+          uid: 3,
+          title: 'Dies ist auch noch eine Überschrift',
+          bodytext: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor mollit anim id est laborum.'
+  }],
+  model: function() {
+      return this.get('posts');
+  },
+  actions: {
+      reversePosts: function(){
+          var posts = this.get('posts');
+          this.set('posts', posts.reverse());
+
+          this.refresh;
+      }
+  }
+});
